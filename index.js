19b25f6f25e7622e7308491d2b92cf964bb08c48
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-03-31 00:34:03 +0530

    added fullscreen functionality & css files

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const username = document.getElementById("name");
 const gravatar = document.getElementById("gravatar");
+const locdisp = document.getElementById("location");
+
+const element = document.getElementById('maindiv');
+
+document.getElementById('submit').addEventListener('click', () => {
+	if (screenfull.isEnabled) {
+		screenfull.request(element);
+        screenfull.request(element, {navigationUI: 'hide'});
+	}
+});
 
 // on click function....
 async function dta () {
@@ -26,7 +36,7 @@ async function dta () {
     // rendering on front end....
 
     // to check for null fields....
-    if (name == null) {
+    if (name == null & loc == null & img == null) {
         username.textContent = "OOPS not found .."
     }
     else {
@@ -34,9 +44,11 @@ async function dta () {
     }
     gravatar.src = img;
     gravatar.alt = inputVal;
+    locdisp.textContent = loc;
+
 
 
     // debug statements....
     console.log(name,loc);
-    alert(inputVal);
+    // alert(inputVal);
 }
