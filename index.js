c6f74069e051527cdc04ffb5a6635ca257a8e9c6
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-04-01 00:51:53 +0530

    added downloadable div, backgrounds and a few needed lines of JS.

--- index.js
+++ index.js
@@ -2,53 +2,71 @@ const username = document.getElementById("name");
 const gravatar = document.getElementById("gravatar");
 const locdisp = document.getElementById("location");
 
-const element = document.getElementById('maindiv');
+const element = document.getElementById("maindiv");
 
-document.getElementById('submit').addEventListener('click', () => {
-	if (screenfull.isEnabled) {
-		screenfull.request(element);
-        screenfull.request(element, {navigationUI: 'hide'});
-	}
-});
+// document.getElementById('submit').addEventListener('click', () => {
+// 	if (screenfull.isEnabled) {
+// 		screenfull.request(element);
+//         screenfull.request(element, {navigationUI: 'hide'});
+// 	}
+// });
 
 // on click function....
-async function dta () {
-    let inputVal = document.getElementById("text").value;
+async function dta() {
+  let inputVal = document.getElementById("text").value;
+  // Making maindiv visible on button click....
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+  if (screenfull.isEnabled) {
+    screenfull.request(element);
+    screenfull.request(element, { navigationUI: "hide" });
+  }
+  // if (element.style.display === "none") {
+  //     element.style.display = "block";
+  //   } else {
+  //     element.style.display = "none";
+  //   }
 
-    // Base url for every request....
-    let baseURL = "https://api.github.com/users/" ;
-    // url to request for ....
-    let usrreq = baseURL+inputVal;
-    // await for the url and getting back the json....
-    const res = await fetch(usrreq);
-    const data = await res.json();
-    //console.log(data);
-    
-    // for name ....
-    let name = (data.name);
+  // Base url for every request....
+  let baseURL = "https://api.github.com/users/";
+  // url to request for ....
+  let usrreq = baseURL + inputVal;
+  // await for the url and getting back the json....
+  const res = await fetch(usrreq);
+  const data = await res.json();
+  //console.log(data);
 
-    // for location ....
-    let loc = (data.location);
+  // for name ....
+  let name = data.name;
 
-    // for gravatar....
-    let img = (data.avatar_url);
-    
-    // rendering on front end....
+  // for location ....
+  let loc = data.location;
 
-    // to check for null fields....
-    if (name == null & loc == null & img == null) {
-        username.textContent = "OOPS not found .."
-    }
-    else {
-        username.textContent = name;
-    }
-    gravatar.src = img;
-    gravatar.alt = inputVal;
-    locdisp.textContent = loc;
+  // for gravatar....
+  let img = data.avatar_url;
 
+  // rendering on front end....
 
+  // to check for null fields....
+  if ((name == null) & (loc == null) & (img == null)) {
+    username.textContent = "OOPS not found ..";
+  } else {
+    username.textContent = name;
+  }
+  gravatar.src = img;
+  gravatar.alt = inputVal;
+  locdisp.textContent = loc;
 
-    // debug statements....
-    console.log(name,loc);
-    // alert(inputVal);
+  // debug statements....
+  console.log(name, loc);
+  // alert(inputVal);
+}
+
+if (screenfull.isEnabled) {
+  screenfull.on("change", () => {
+    console.log("SCreen detected");
+  });
 }
