d89156ab6e2b4d8f63a6e98e112d5c1335d8875f
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-05-25 23:40:33 +0530

    resolving img deployment errors

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ async function dta() {
     console.log(totalstars);
     let staricon = document.createElement("img");
     staricon.src =
-      "https://github.com/code-reaper08/G-Card/blob/website/img/star.png?raw=true";
+      "./img/star.png";
     staricon.alt = "Total stars";
     staricon.className = "staricon";
     stardiv.appendChild(stariconcontainerdiv);
